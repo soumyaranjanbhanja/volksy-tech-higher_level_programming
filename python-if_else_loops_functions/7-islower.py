fa4dc94908bccf3b4f9f7ffb47c44b56438ed5fe
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-02 04:24:00 -0800

    seventh

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if c is lowercase:
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
-    else:
-        return False
+    return False
