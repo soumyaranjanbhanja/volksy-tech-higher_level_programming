490ba228b023f3c22c987c3d9f8bf6c168c51121
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-05 04:06:06 -0800

    one

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        my_list.reverse()
+        for i in my_list:
+               print('{:d}'.format(i))
+    else:
+        return None
