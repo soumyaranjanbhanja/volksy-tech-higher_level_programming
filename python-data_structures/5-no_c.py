d658a0ea952fd39c8ceba26cfe2edfb999498adf
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-06 02:28:54 -0800

    five

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    remove_c = ""
+    for index in range(len(my_string)):
+        if (my_string[index] != 'c' and my_string[index] != 'C'):
+            remove_c += my_string[index]
+    return remove_c        
