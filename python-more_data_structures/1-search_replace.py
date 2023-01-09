189c47ba9da24679db1126abbb010bf76fbf1d57
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-09 01:32:48 -0800

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-my_list=[1,2,3,4,5]
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return (my_list)
+    return [[num, replace][num is search] for num in my_list]
