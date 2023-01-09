7bc2c423316b67133b0dddd33ceb1b6eaa997788
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-08 23:48:49 -0800

    2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return (sum(set(my_list)))
+    new_list = set(my_list)
+    result = 0
+    for element in new_list:
+        result += element
+    return result
