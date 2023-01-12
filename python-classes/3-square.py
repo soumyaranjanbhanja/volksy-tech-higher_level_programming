fe63c15553aa488b2826ac65be54290915aba8a1
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-11 21:23:29 -0800

    2

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,6 +11,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    
     def area(self):
         ar = self.__size * self.__size
         return ar
