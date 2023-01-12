f171431896e50dd0d7d9d19479e606bc702682d4
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-12 02:27:29 -0800

    five

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""class"""
 
 
 class Square:
-    """Represent a square."""
+    """class Square"""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be  an integer")
-        elif size < 0:
+        """constructer"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Decorator"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Decorator"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        a = self.__size * self.__size
+        return a
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
