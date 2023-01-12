f051547da1f99488bff8ce04a2a43467095daf75
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-11 21:17:47 -0800

    2

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class"""
+
+:wq!
+class Square:
+    """Square is class"""
+    def __init__(self, size=0):
+        """Constructer"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        ar = self.__size * self.__size
+        return ar
