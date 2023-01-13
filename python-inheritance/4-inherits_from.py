6cafebeaaa773f75aa5e1106ee4ab8aa2fab127d
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-13 03:48:17 -0800

    2

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-
-"""
-inherits_from Module
+"""inherits_from
 """
 
 
 def inherits_from(obj, a_class):
-    """Returns True if the object is an instance of the class that inherited from"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    """Function that returns True if the object is an instance
+    of a class that inherited (directly or indirectly) from the
+    specified class; otherwise False.
+    """
+
+    if type(obj) is a_class or not isinstance(obj, a_class):
+        return False
+    else:
+        return True
