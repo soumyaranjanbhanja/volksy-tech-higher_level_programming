b1752115bd6c8d6baeabca91e8996432c9420a32
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-13 04:16:14 -0800

    2

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 
 """
-BaseGeometry/Rectangle Module
+BaseGeometry Module
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+class BaseGeometry():
+    """BaseGeometry class"""
 
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
+    def area(self):
+        """Raises an Exception if it is not implemented"""
+        raise Exception("area() is not implemented")
 
-    def __init__(self, width, height):
-        """Initalization of the class"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    def integer_validator(self, name, value):
+        """Value Validation"""
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
