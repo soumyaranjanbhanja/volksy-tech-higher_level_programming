1079df6a2cc5ecf6083521d63a8bdf9b65373f42
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-16 05:04:37 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+
+
 def number_of_lines(filename=""):
-    count = 0
-    with open(filename, encoding="utf-8") as fd:
-        for line in fd:
-            count += 1
-    return count
+    with open(filename, "r", encoding="UTF-8") as f:
+        return len(list(f))
