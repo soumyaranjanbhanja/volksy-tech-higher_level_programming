8368f71d90d2b028a1459422d2466bd3b17b33fa
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-18 02:57:29 -0800

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+""" This module that contains a function which reads from a file """
+
+
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read())
+    """
+    Function that reads from a file
+    Args:
+        filename: filename
+    Raises:
+        Exception: when the file can be opened
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
