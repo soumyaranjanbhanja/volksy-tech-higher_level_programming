eeb358c120a3b75261067c3ee8bd8131b2990038
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-18 20:41:55 -0800

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+
+"""
+
+Script that adds all arguments to a Python list,
+
+then saves them to a file
+
+"""
+
 import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+
+
+fname = "add_item.json"
+
+ListyBoii = []
+
+
+
+
+try:
+
+    ListyBoii = load_from_json_file(fname)
+
+except:
+
+    pass
+
+
+
+
+for i in range(1, len(sys.argv)):
+
+    ListyBoii.append(sys.argv[i])
+
+save_to_json_file(ListyBoii, fname)
