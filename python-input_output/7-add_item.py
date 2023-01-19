c721ebca9a40e6081af0927d2a9323de67eed93f
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-19 05:06:51 -0800

    task-7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-Save Module
-"""
+"""here documented"""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """write an object to a text file
-    args:
-        my_obj: object to manipulate
-    filename:
-        filename: text file
-    return:
-        object
-    """
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file.py').load_from_json_file
 
-    with open(filename, "w", encoding="utf-8") as f:
-        written = json.dump(my_obj, f)
-    return written
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
