b7bb2901b7a847e8e0e33864a133c8299280e3a2
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-29 23:01:51 -0800

    4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- script that creates table first table in database
-CREATE TABLE IF NOT EXISTS first_table(id int,name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table(id INT,name VARCHAR(256));
