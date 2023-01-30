c2fe85df7504b884b7f10459cae0798dd2af7da3
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-01-30 01:18:04 -0800

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,2 @@
--- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
-SHOW CREATE TABLE first_table`(
-`id`intNOTNULLAUTO_INCREMENT,
-`name`varchar(128)DEFAULTNULL,
-`c`char(1)DEFAULTNULL,
-`created_at`dateDEFAULTNULL,
-PRIMARYKEY(`id`)
-)
+-- ints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+SHOW CREATE TABLE first_table;
