fe81cd109ceef85b945ec92473dd109000353cc2
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-02-06 04:11:47 -0800

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,2 @@
--- Script that creates the MySQL server user:
--- user_0d_1 with all privileges and certain password.
-CREATE USER user_0d_1@localhost
-IDENTIFIED BY 'user_0d_1_pwd';
--- all databases . all tables
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+-- Creates the user user_0d_1
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
