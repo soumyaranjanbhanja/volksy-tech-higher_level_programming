bdd34a0da1013d23e5d177e17f107f3935ac2b1b
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-02-06 03:48:07 -0800

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,17 +1,8 @@
--- Script that creates the database hbtn_0d_usa and the table states
--- in the database hbtn_0d_usa on your MySQL server.
--- id = INT auto generated not null which is primary key and must be unique.
--- name = VARCHAR not null of length: 256.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
-(
--- Script that creates the database hbtn_0d_usa and the table states
--- in the database hbtn_0d_usa on your MySQL server.
--- id = INT auto generated not null which is primary key and must be unique.
--- name = VARCHAR not null of length: 256.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
-(
-    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
+-- Create table and database if not exist
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE table IF NOT EXISTS cities
+       (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id));
