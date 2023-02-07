dbc77540a747cc48bdd99fd67de34bb932cc183d
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-02-06 21:02:11 -0800

    5-unique_id.sql

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
--- Script that creates the table id_not_null on your MySQL server.
--- id = INT with default value of 1.
--- name = VARCHAR of length: 256.
-CREATE TABLE IF NOT EXISTS id_not_null
-(id INT DEFAULT '1', name VARCHAR(256));
+-- Creates the table unique_id
+CREATE TABLE IF NOT EXISTS unique_id (
+       id INT DEFAULT 1 UNIQUE,
+       name VARCHAR(256)
+);
