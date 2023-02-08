a9c362e11cb5b2c165826fc293d0ca67a6387b19
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2023-02-08 01:14:51 -0800

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa
-using certain database configuration specified via cmdline args.
-"""
-
-import sys
-import MySQLdb as connector
-
+"""lists all states from the database"""
+import MySQLdb
+from sys import argv
 if __name__ == "__main__":
-
-    [db_username, db_password, db_name] = sys.argv[1:]
-
-    connection_configuration = {
-        'host': 'localhost',
-        'user': db_username,
-        'passwd': db_password,
-        'db': db_name,
-        'port': 3306
-    }
-
-    database = connector.connect(**connection_configuration)
-    cursor = database.cursor()
-
-    query = 'SELECT * FROM states ORDER BY states.id ASC'
-    cursor.execute(query)
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
-    cursor.close()
-    database.close()
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    for i in range(num_rows):
+        print(cur.fetchone())
