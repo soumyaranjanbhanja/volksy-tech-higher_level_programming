304a42de128a137b37ed75171b59644fa3f01816
Your Name <you@example.com>
2023-03-01 05:16:41 -0800

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = "https://swapi-api.hbtn.io/api/films/:id" + process.argv[2];
 let resultDict = {};
 request(url, function (error, response, body) {
   if (error) {
